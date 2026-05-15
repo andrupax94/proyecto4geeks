02e0b4682e190fa97791a0747d251c61cfec2cf8
alemantellini <mantellinialessandra13@gmail.com>
2026-05-15 22:26:44 +0200

    Añadir nuevos enlaces de recursos de dataset y crear archivo de metadatos

--- src/data/build/AudioSet/audioset.py
+++ src/data/build/AudioSet/audioset.py
@@ -192,31 +192,6 @@ class AudioSet:
 if __name__ == "__main__":
     script_dir =  BUILD_DIR / "audioset"
     file_list = {"audioset.csv":["audioset.csv",0.8]}
-    # df_res={}
-    # for key,val in file_list.items():
-    #     df_original = pd.read_csv(os.path.join(script_dir, val[0]))
-    #     # Clases que quieres conservar (ahora puedes usar los nombres oficiales)
-        
-    #     nombre_salida = os.path.join(RAW_DIR, key)
-    #     df_res[key] = Dataset.generar_csv_audio(
-    #         df=df_original,
-    #         col_labels='category',
-    #         col_mids=RAW_DIR / "zenodo.csv",
-    #         col_fname='filename',
-    #         ruta_carpeta="ESC50/",
-    #         sinonimos=None,
-    #         nombre_salida=nombre_salida,
-    #         dataset_name="ESC50",
-    #         split=val[1]
-    #     )
-    #     print(df_res[key].head())
-    # =====================================
-# FUNCTIONS
-# =====================================
-
-# =====================================
-# LOAD AUX FILES
-# =====================================
 
     print("Cargando sinonimos...")
     sinonimos = pd.read_csv(SINONIMOS_V3_PATH)
