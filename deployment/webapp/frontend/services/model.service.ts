757cbd1f8dea50804edc73601b63b66986b1944a
natisbp <nbayona2112@gmail.com>
2026-05-20 14:08:20 -0500

    refactor estructura despliegue

--- deployment/webapp/frontend/services/model.service.ts
+++
@@ -1,5 +0,0 @@
-import { apiGet } from './api';
-
-export function getModelSummary() {
-  return apiGet('/model/summary');
-}
--- deployment/webapp/frontend/services/sound.service.ts
+++
@@ -1,5 +0,0 @@
-import { apiGet } from './api';
-
-export function getSounds() {
-  return apiGet('/sounds');
-}
