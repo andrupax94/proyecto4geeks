7f4a5a69a95549a12e7125d2208ebd196a015314
alemantellini <mantellinialessandra13@gmail.com>
2026-05-22 19:31:12 +0200

    Prueba

--- deployment/backend/app/models/model_loader.py
+++ deployment/backend/app/models/model_loader.py
@@ -4,9 +4,12 @@ from src.models.hybrid_cnn_v3 import ImprovedMFCCCNN
 
 MODEL_PATH = "models/final/model_total_V4_32.pt"
 
+mode = "mel_waveform"
+NUM_CLASSES = 2
+
 device = torch.device("cpu")
 
-"""model = ImprovedMFCCCNN()
+model = ImprovedMFCCCNN(num_classes=NUM_CLASSES, dropout=0.25, mode=mode).to(device)
 
 checkpoint = torch.load(
     MODEL_PATH,
@@ -15,4 +18,4 @@ checkpoint = torch.load(
 
 model.load_state_dict(checkpoint)
 
-model.eval()"""
+model.eval()
