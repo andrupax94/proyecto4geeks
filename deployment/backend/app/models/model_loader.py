08259266c852fcade42e74f850c2b50f9469139a
natisbp <nbayona2112@gmail.com>
2026-05-22 13:07:57 -0500

    model_loader

--- deployment/backend/app/models/model_loader.py
+++ deployment/backend/app/models/model_loader.py
@@ -1,15 +1,27 @@
+from pathlib import Path
+import sys
 import torch
+
+# raíz del proyecto
+BASE_DIR = Path(__file__).resolve().parents[4]
+
+# agregar proyecto4geeks al path
+sys.path.append(str(BASE_DIR))
 
 from src.models.hybrid_cnn_v3 import ImprovedMFCCCNN
 
-MODEL_PATH = "models/final/model_total_V4_32.pt"
+MODEL_PATH = BASE_DIR / "models" / "final" / "best_alertable_v3.pt"
 
 device = torch.device("cpu")
 
-"""mode = "mel_waveform"
+mode = "mel_waveform"
 NUM_CLASSES = 2
 
-model = ImprovedMFCCCNN(num_classes=NUM_CLASSES, dropout=0.25, mode=mode).to(device)
+model = ImprovedMFCCCNN(
+    num_classes=NUM_CLASSES,
+    dropout=0.25,
+    mode=mode
+).to(device)
 
 checkpoint = torch.load(
     MODEL_PATH,
@@ -18,4 +30,4 @@ checkpoint = torch.load(
 
 model.load_state_dict(checkpoint)
 
-model.eval()"""
+model.eval()
